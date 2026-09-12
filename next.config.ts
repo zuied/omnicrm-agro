c124b4f0ea42ae21dfe1e833d3ed0a8fe0bb04d7
zuied <zuied69@gmail.com>
2026-09-12 22:55:18 +0800

    Perbaiki LAN dev: allowedDevOrigins + alignment schema

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["http://localhost:3111", "http://192.168.1.9:3111", "http://192.168.*:3111"],
 };
 
 export default nextConfig;
